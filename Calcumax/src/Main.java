3270361dd5fffd5ae47d23c3bfc32e512dfa3d20
Emile Delmas <emile.delmas@gmail.com>
2023-10-06 14:48:38 +0200

    Add Deque and test the stack

--- Calcumax/src/Main.java
+++ Calcumax/src/Main.java
@@ -1,8 +1,15 @@
-
+// Import scanner
+import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
         System.out.printf("Hello and welcome to Calcumax!");
-        
+
+		// Create a new instance of the class Modele
+		Modele modele = new Modele();
+		// Create an input for the user
+		Scanner sc = new Scanner(System.in);
+		// Send it to the modele
+		modele.add_double_to_deque(sc.nextDouble());
 	}
 }
--- Calcumax/src/Modele.java
+++ Calcumax/src/Modele.java
@@ -1,5 +1,32 @@
+// Import Deque
+import java.util.Deque;
+// Import ArrayDeque
+import java.util.ArrayDeque;
+
 
 public class Modele {
+    // Create a deque of float
+    private Deque<Double> deque;
+    private double res;
+    
+    // Constructor
+    public Modele() {
+            deque = new ArrayDeque<Double>();
+            res = 0;
+    }
+
+    // Create a getter for the res
+    public double get_res() {
+    	return res;
+    }
+
+    // Fonction to add a number to the deque
+    public void add_double_to_deque(double i) {
+        	deque.add((double) i);
+            System.out.println(deque);
+    }
+
+    //
 
     
 }
