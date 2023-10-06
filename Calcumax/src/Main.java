15fc055966ba538547e5b9d71106ee126d0336d1
Emile Delmas <emile.delmas@gmail.com>
2023-10-06 15:50:54 +0200

    Add operation function and made it operational

--- Calcumax/src/Main.java
+++ Calcumax/src/Main.java
@@ -3,13 +3,45 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
-        System.out.printf("Hello and welcome to Calcumax!");
+        System.out.println("Hello and welcome to Calcumax!");
 
 		// Create a new instance of the class Modele
 		Modele modele = new Modele();
+		
+		while (true) {
+	     System.out.println("give a number:");
+
 		// Create an input for the user
 		Scanner sc = new Scanner(System.in);
-		// Send it to the modele
-		modele.add_double_to_deque(sc.nextDouble());
+		//if it's a double
+		if (sc.hasNextDouble()) {
+			// Add the double to the deque
+			modele.add_double_to_deque(sc.nextDouble());
+		}
+		//else if it's a string
+		else {
+			// if it's a +
+			if (sc.hasNext("\\+")) {
+				modele.operation(1);
+			}
+			// if it's a -
+			else if (sc.hasNext("-")) {
+				modele.operation(2);
+			}
+			// if it's a *
+			else if (sc.hasNext("\\*")) {
+				modele.operation(3);
+			}
+			// if it's a /
+			else if (sc.hasNext("/")) {
+				modele.operation(4);
+			}
+			else {
+				System.out.println("Not a valid input");
+			}
+		}
+		System.out.println(modele.get_res());
+
+	}
 	}
 }
--- Calcumax/src/Modele.java
+++ Calcumax/src/Modele.java
@@ -3,7 +3,17 @@ import java.util.Deque;
 // Import ArrayDeque
 import java.util.ArrayDeque;
 
+/*TODO
+- Change int i in operation to String "+"
+- Backspace
+- Clear
+- Add a button to change the sign of the number
+- push(), pop(), drop(), swap(), clear() and add()
+- Divide by 0 rule
+- enter number digit by digit in a string and concatenate (test every time if it's a number)
+- ",5" = 0.5
 
+*/
 public class Modele {
     // Create a deque of float
     private Deque<Double> deque;
@@ -23,10 +33,40 @@ public class Modele {
     // Fonction to add a number to the deque
     public void add_double_to_deque(double i) {
         	deque.add((double) i);
-            System.out.println(deque);
     }
 
-    //
+    public void operation(int i){
+            // if deque length is more than 2
+            if (deque.size() > 1) {
+                // if i is 1,2,3 or 4
+                if (i > 0 && i < 5){
+                    double n1 = deque.pop();
+                    double n2 = deque.pop();
 
+                    if (i == 1){
+                        // Add the two last numbers of the deque
+                        res = n1 + n2;
+                    }
+                    else if (i == 2) {
+                        // Substract the two last numbers of the deque
+                        res = n1 - n2;
+                    }
+                    else if (i == 3) {
+                        // multiply the two last numbers of the deque
+                        res = n1 * n2;
+                    }
+                    else if (i == 4) {
+                        // Divide the two last numbers of the deque
+                        res = n1 / n2;
+                    }
+                    deque.add(res);
+                }
+             
+                // Add the two last numbers of the deque
+            }
+            else {
+                System.out.println("Not enough numbers in the deque");
+            }
+    }
     
 }
