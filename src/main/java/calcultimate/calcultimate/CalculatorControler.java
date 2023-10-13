dffca9e079bea20588d8d576ce32f2d9258dd38c
Emile Delmas <emile.delmas@gmail.com>
2023-10-13 16:09:15 +0200

    Rename class, create interfaces

--- src/main/java/calcultimate/calcultimate/CalculatorControler.java
+++ src/main/java/calcultimate/calcultimate/CalculatorControler.java
@@ -1,6 +1,11 @@
 package calcultimate.calcultimate;
 
+import java.util.EventObject;
 import java.util.List;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 
 public class CalculatorControler implements CalculatorControlerInterface{
     @Override
@@ -11,5 +16,32 @@ public class CalculatorControler implements CalculatorControlerInterface{
     @Override
     public void change(List<Double> StackData) {
 
+    }
+
+
+
+    @FXML
+    private Label welcomeText;
+    private EventObject event;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void handleButtonAction() {
+        //Get text from the button where the event happened
+        String text = ((Button)event.getSource()).getText();
+        welcomeText.setText(text);
+    }
+
+    @FXML
+    protected void handleClearAction() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void handleEqualsAction() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
---
+++ src/main/java/calcultimate/calcultimate/CalculatorGuiInterface.java
@@ -0,0 +1,9 @@
+package calcultimate.calcultimate;
+
+import java.util.List;
+
+public interface CalculatorGuiInterface {
+    public void affiche();
+    public void change(String accu);
+    public void change(List<Double> StackData);
+}
