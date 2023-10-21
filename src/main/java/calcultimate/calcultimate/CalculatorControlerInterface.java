7176762079648c319e7bc890e4546c75f6b099f0
emiledelmas <emile.delmas@gmail.com>
2023-10-21 10:39:49 +0200

    Fix the CalculatorControlerInterface

--- src/main/java/calcultimate/calcultimate/CalculatorControlerInterface.java
+++ src/main/java/calcultimate/calcultimate/CalculatorControlerInterface.java
@@ -3,10 +3,6 @@ package calcultimate.calcultimate;
 import java.util.Stack;
 
 public interface CalculatorControlerInterface {
-    public void change(String accu);
-
-    public void change(Stack<Double> StackData);
-
-
+    public void change();
 
 }
